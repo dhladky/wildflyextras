80e24fe7926dee547924e1dd2bdda284a6e78772
David Hladky <dhladky@redhat.com>
2014-09-05 16:49:08 +0200

    Initial commit

---
+++ src/main/java/SomeClass.java
@@ -0,0 +1,14 @@
+/** This is a test class for deploying to org.wildfly.extras. Do not expect it to do anything useful
+ *
+ * <br/> <br/>
+ * User: dhladky@redhat.com <br/>
+ * Date: 05/09/14 <br/>
+ * Time: 14:51  <br/>
+ */
+public class SomeClass {
+
+    public static void main(String arguments[]) {
+        System.out.printf("Wildfly Extas Deployment Test");
+    }
+
+}
